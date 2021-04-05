0c1038b6cfa9f85f890f2ab68716bd5549954f0a
vrana99 <fvranaa@gmail.com>
2021-04-05 19:20:10 -0300

    configuracion heroku

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 
 const publicPath = path.resolve(__dirname, './public') ;
+const puerto= process.env.PORT;
+
 app.use(express.static(publicPath));
 
 app.get('/', (req,res) => {
@@ -15,6 +17,6 @@ app.get('/login.html', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'));
 });
 
-app.listen(3000 , () => {
-    console.log('El servidor de Facundo Vrana esta corriendo en el puerto 3000');
+app.listen(puerto || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
 });
